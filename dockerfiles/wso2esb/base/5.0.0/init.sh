1c42553dcd890775a15d4a76ab2ca281736ea67f
Maheeka Jayasuriya <maheeka@wso2.com>
2016-08-21 23:31:37 +0530

    tenant support

--- dockerfiles/wso2esb/base/5.0.0/init.sh
+++ dockerfiles/wso2esb/base/5.0.0/init.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
-CARBON_HOME_PATH=/opt/wso2esb-5.0.0
+CARBON_HOME_PATH=/home/wso2user/wso2esb-5.0.0
 #remove default java opts
 sed -i '/-Xms256m/d' $CARBON_HOME_PATH/bin/wso2server.sh
 
-sed -i "/port=\"9763\"/a  \\\t\t   proxyPort=\"80\"" $CARBON_HOME_PATH/repository/conf/tomcat/catalina-server.xml
-sed -i "/port=\"9443\"/a  \\\t\t   proxyPort=\"443\"" $CARBON_HOME_PATH/repository/conf/tomcat/catalina-server.xml
+sed -i "/port=\"9763\"/a    proxyPort=\"80\"" $CARBON_HOME_PATH/repository/conf/tomcat/catalina-server.xml
+sed -i "/port=\"9443\"/a    proxyPort=\"443\"" $CARBON_HOME_PATH/repository/conf/tomcat/catalina-server.xml
 #sed -i '/<WebContextRoot>/c\\t<WebContextRoot>/dss</WebContextRoot>' $CARBON_HOME_PATH/repository/conf/carbon.xml
 
 #Changing admin password
-if [ -z ${ADMIN_PASSWORD+x} ]; then
-    echo "ADMIN_PASSWORD is not set.";
-    echo "Generating admin password.";
-    ADMIN_PASSWORD=${ADMIN_PASS:-$(pwgen -s 12 1)}
-    echo "========================================================================="
-    echo "Credentials for the instance:"
-    echo
-    echo "    user name: admin"
-    echo "    password : $ADMIN_PASSWORD"
-    echo "========================================================================="
-    sed -i "s/.*<Password>admin<\/Password>.*/<Password>$ADMIN_PASSWORD<\/Password>/" $CARBON_HOME_PATH/repository/conf/user-mgt.xml
-else
-    echo "ADMIN_PASSWORD set by user.";
-fi
+#if [ -z ${ADMIN_PASSWORD+x} ]; then
+#    echo "ADMIN_PASSWORD is not set.";
+#    echo "Generating admin password.";
+#    ADMIN_PASSWORD=${ADMIN_PASS:-$(pwgen -s 12 1)}
+#    echo "========================================================================="
+#    echo "Credentials for the instance:"
+#    echo
+#    echo "    user name: admin"
+#    echo "    password : $ADMIN_PASSWORD"
+#    echo "========================================================================="
+#    sed -i "s/.*<Password>admin<\/Password>.*/<Password>$ADMIN_PASSWORD<\/Password>/" $CARBON_HOME_PATH/repository/conf/user-mgt.xml
+#else
+#    echo "ADMIN_PASSWORD set by user.";
+#fi
 
 #Remove bundles from plugins dir and the bundles.info to minimize jaggery runtime
 PLUGINS_DIR_PATH="$CARBON_HOME_PATH/repository/components/plugins/"
